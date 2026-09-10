d65ea25ff9e00fb4ed9f7f607b1b6ada23e1a2dc
Mohamed Elsaid <ap56store@gmail.com>
2026-09-11 01:43:27 +0300

    Fix hero slider images

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -7,6 +7,13 @@ import 'swiper/css/pagination';
 import { Autoplay, Pagination } from 'swiper/modules';
 import { Link } from 'react-router-dom';
 
+import bannerHero1 from '../img/banner_Hero1.jpg';
+import bannerHero2 from '../img/banner_Hero2.jpg';
+import bannerHero3 from '../img/banner_Hero3.jpg';
+
+
+
+
 function HeroSlider() {
     return (
         <>
@@ -28,7 +35,8 @@ function HeroSlider() {
                                 <p>Windows Xp/10/7/8 ps3,Tv Box</p>
                                 <Link to="/" className='btn'>Shop Now</Link>
                             </div>
-                            <img src="/src/img/banner_Hero1.jpg" alt="slider hero 1" />
+                            
+                            <img src={bannerHero1} alt="slider hero 1" />
                         </SwiperSlide>
 
                         <SwiperSlide>
@@ -38,7 +46,7 @@ function HeroSlider() {
                                 <p>Windows Xp/10/7/8 ps3,Tv Box</p>
                                 <Link to="/" className='btn'>Shop Now</Link>
                             </div>
-                            <img src="/src/img/banner_Hero2.jpg" alt="slider hero 1" />
+                            <img src={bannerHero2} alt="slider hero 1" />
                         </SwiperSlide>
 
                         <SwiperSlide>
@@ -48,7 +56,7 @@ function HeroSlider() {
                                 <p>Windows Xp/10/7/8 ps3,Tv Box</p>
                                 <Link to="/" className='btn'>Shop Now</Link>
                             </div>
-                            <img src="/src/img/banner_Hero3.jpg" alt="slider hero 1" />
+                            <img src={bannerHero3} alt="slider hero 1" />
                         </SwiperSlide>
 
                     </Swiper>
